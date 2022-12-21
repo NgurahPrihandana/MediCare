3919468ef81757aa4520e5252b6e55af829bfb38
srimuspani <99719200+srimuspani@users.noreply.github.com>
2022-12-21 15:59:25 +0800

    Menambahkan High Qualified Doctors

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -122,8 +122,118 @@
     <div class="container mt-5">
       <div class="row">
         <div class="contact-container">
-          
+          <h3 align="center">High Qualified Doctors </h3> <br>
         </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="card-doctor">
+            <div class="row">
+              <div class="col-lg-5"><img src="img/doctor1.png" alt=""></div>
+              <div class="col-lg-7">
+                <div class="card-doctor-title">
+                  <h4>Annisa Sahda Devina</h4>
+                  <p>Neurologist</p>
+                </div>
+
+                <div class="card-doctor-body">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil officia quas iste consequatur aliquid distinctio temporibus maiores quaerat sequi! Deserunt.
+                  </p>
+                  <a href="">More about Annisa</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="card-doctor">
+            <div class="row">
+              <div class="col-lg-5"><img src="img/doctor1.png" alt=""></div>
+              <div class="col-lg-7">
+                <div class="card-doctor-title">
+                  <h4>Mutiara Orlanda H.</h4>
+                  <p>Dentist</p>
+                </div>
+
+                <div class="card-doctor-body">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil officia quas iste consequatur aliquid distinctio temporibus maiores quaerat sequi! Deserunt.
+                  </p>
+                  <a href="">More about Mutiara</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6 mt-5">
+          <div class="card-doctor">
+            <div class="row">
+              <div class="col-lg-5"><img src="img/doctor1.png" alt=""></div>
+              <div class="col-lg-7">
+                <div class="card-doctor-title">
+                  <h4>Anis Zahra Nur Azizah</h4>
+                  <p>Pediatric</p>
+                </div>
+
+                <div class="card-doctor-body">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil officia quas iste consequatur aliquid distinctio temporibus maiores quaerat sequi! Deserunt.
+                  </p>
+                  <a href="">More about Anis</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 mt-5">
+          <div class="card-doctor">
+            <div class="row">
+              <div class="col-lg-5"><img src="img/doctor1.png" alt=""></div>
+              <div class="col-lg-7">
+                <div class="card-doctor-title">
+                  <h4>Triska Widiantari</h4>
+                  <p>Opthalmologist</p>
+                </div>
+
+                <div class="card-doctor-body">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil officia quas iste consequatur aliquid distinctio temporibus maiores quaerat sequi! Deserunt.
+                  </p>
+                  <a href="">More about Triska</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="mt-5">
+          <div class="card-contact">
+            <div class="row">
+              <div class="col-lg-9">
+                <h3>Contact us to know more about MediCare</h3>
+                <h4>Emergency medical care</h4>
+              </div>
+              <div class="col-lg-3 d-flex align-items-center">
+                <a href="" class="btn-contact-us">Contact Us</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="mt-5">
+        <img src="img/logo-medicare.png" alt="Logo">
+        <span class="text-logo">MediCare</span>
       </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
